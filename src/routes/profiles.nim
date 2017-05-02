61ad36455c8d526fc5df455089b8c23da9ac6f4b
Attila Bagossy <bagossyattila@outlook.com>
2017-05-03 01:34:18 +0200

    Implemented the first profile route

--- src/routes/profiles.nim
+++ src/routes/profiles.nim
@@ -1,12 +1,25 @@
 # Although system is implicitly imported, we now want to
 # explicitly exclude the delete procedure
 import system except delete
+import json except delete
+import asyncdispatch, oids
 
 import rosencrantz
 
 import ../model/user
+import ../service/profileservice
 from filter/auth import mandatoryAuth, optionalAuth
 
+
+proc respondWithProfile(profile: Profile): Handler =
+  let profileJson = %*{
+    "username": profile.username,
+    "bio": profile.bio,
+    "image": profile.image,
+    "following": profile.following
+  }
+
+  ok(%{ "profile": profileJson })
 
 let
   getProfile =
@@ -14,7 +27,20 @@ let
       pathChunk("/api/profiles") ->
         segment do (username: string) -> auto:
           optionalAuth do (user: User) -> auto:
-            ok("Get Profile: " & username)
+            scopeAsync do:
+              let profileFut = getByUsername(username)
+
+              yield profileFut
+
+              if profileFut.failed():
+                return notFound("Profile Not Found!")
+              else:
+                let profile = profileFut.read()
+
+                if user != nil:
+                  profile.following = profile.id in user.following
+
+                return respondWithProfile(profile)
 
   followUser =
     post ->
--- src/service/profileservice.nim
+++ src/service/profileservice.nim
@@ -1,19 +1,34 @@
-from ../model/user import User
+import asyncdispatch, oids
 
+from ../model/user import User
+from ../util/future import completed, failed
+from userservice import getByUsername
 
 type
   Profile* = ref object
+    id*: Oid
     username*: string
     bio*: string
     image*: string
     following*: bool
 
-proc profileFromUser(user: User): Profile =
+  ProfileNotFoundError* = object of Exception
+
+converter userToProfile(user: User): Profile =
   result.new
+  result.id = user.id
   result.username = user.username
   result.bio = user.bio
   result.image = user.image
   result.following = false
 
-proc getByUsername(username: String): Profile =
-  discard
+proc getByUsername*(username: string): Future[Profile] {.async.} =
+  let
+    userFut = userservice.getByUsername(username)
+
+  yield userFut
+
+  if (userFut.failed()):
+    return await failed[Profile](newException(ProfileNotFoundError, "Profile Not Found!"))
+  else:
+    return await completed(userFut.read())
